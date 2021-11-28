56442a507b8ff5181cdec62d55f8a0a63e36ed2b
unknown <programador@SECEJEC01.iepcjalisco.org.mx>
2021-11-27 23:03:36 -0600

    Se crean las funciones para monitorear directorios con timer y consumir servicios y llenar tabla con lista

---
+++ MonitoreoFilesDirectorio.Designer.cs
@@ -0,0 +1,70 @@
+﻿
+namespace Digitalizador
+{
+    partial class MonitoreoFilesDirectorio
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 5000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(2, 0);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(224, 441);
+            this.textBox1.TabIndex = 1;
+            // 
+            // MonitoreoFilesDirectorio
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(226, 441);
+            this.Controls.Add(this.textBox1);
+            this.Name = "MonitoreoFilesDirectorio";
+            this.Text = "MonitoreoFilesDirectorio";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
---
+++ MonitoreoFilesDirectorio.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Digitalizador
+{
+    public partial class MonitoreoFilesDirectorio : Form
+    {
+        public MonitoreoFilesDirectorio()
+        {
+            InitializeComponent();
+        }
+
+        //public ListViewItem ObtenerFilesDirectorio()
+        //{
+        //    ListViewItem lista = new ListViewItem();
+
+        //    string[] files = Directory.GetFiles(@"C:\files\", "*.txt");
+
+        //    foreach (var file in files)
+        //    {
+        //        listView1.Items.Add(Environment.NewLine);
+        //        listView1.Items.Add(file);
+        //    }
+
+        //    return lista;
+        //}
+
+        //public void limpiarListView()
+        //{
+        //    listView1.Clear();
+        //    listView1.Refresh();
+        //}
+
+        public void LlenarTextBox()
+        {
+            textBox1.Clear();
+            textBox1.Refresh();
+
+            string[] files = Directory.GetFiles(@"C:\files\", "*.txt");
+
+            foreach (var file in files)
+            {
+                textBox1.AppendText(Environment.NewLine);
+                textBox1.AppendText(file);
+            }
+            //textBox1.Refresh();
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //limpiarListView();
+
+            //listView1.Items.Add(ObtenerFilesDirectorio());
+            //listView1.Refresh();
+
+            LlenarTextBox();
+        }
+    }
+}
